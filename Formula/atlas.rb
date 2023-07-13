5b4a0f5d4a27c0bcac70c5a9fe49c96ab6e28b51
Jannik Clausen <github@masseelch.de>
2023-07-13 11:18:55 +0200

    support for arm

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -3,25 +3,29 @@ class Atlas < Formula
   homepage "https://atlasgo.io"
 
   # technique to pull sha and version from HTTP inspired by: https://github.com/pointlessone/homebrew-rust-nightly
-  def self.sha256_checksum (platform)
-    `curl --silent https://release.ariga.io/atlas/atlas-#{platform}-amd64-latest.zip.sha256`.split.first
+  def self.sha256_checksum
+    `curl --silent https://release.ariga.io/atlas/atlas-#{self.os}-#{self.arch}-latest.zip.sha256`.split.first
   end
 
-  def self.latest_version (platform)
-     `curl --silent https://release.ariga.io/atlas/atlas-#{platform}-amd64-latest.version`.split.first
+  def self.latest_version
+    `curl --silent https://release.ariga.io/atlas/atlas-#{self.os}-#{self.arch}-latest.version`.split.first
   end
 
-  if OS.mac?
-    url "https://release.ariga.io/atlas/atlas-darwin-amd64-latest.zip"
-    sha256 self.sha256_checksum "darwin"
-    version self.latest_version "darwin"
+  def self.os
+    if OS.mac?
+      "darwin"
+    elsif OS.linux?
+      "linux"
+    end
   end
 
-  if OS.linux?
-    url "https://release.ariga.io/atlas/atlas-linux-amd64-latest.zip"
-    sha256 self.sha256_checksum "linux"
-    version self.latest_version "linux"
+  def self.arch
+    Hardware::CPU.arm? ? "arm64" : "amd64"
   end
+
+  url "https://release.ariga.io/atlas/atlas-#{self.os}-#{self.arch}-latest.zip"
+  sha256 self.sha256_checksum
+  version self.latest_version
 
   def install
     bin.install "atlas"
